b63de59600d867ae1346925e1b66238eedbcf6d8
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-18 23:59:41 +0200

    Basic view for question

---
+++ Quiz/Views/QuestionsView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionsView.swift
+//  Quiz
+//
+//  Created by Natalia Nikiforuk on 18/10/2024.
+//
+
+import SwiftUI
+
+struct QuestionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionsView()
+}
