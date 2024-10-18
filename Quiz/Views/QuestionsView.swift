813da0bab35d5abc218af2c66191db78d615d595
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-19 00:43:16 +0200

    Game quiz

--- Quiz/Views/QuestionsView.swift
+++ Quiz/Views/QuestionsView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct QuestionsView: View {
     @State private var currentQuestionIndex = 0
+    @State private var questionNr = 1
+    @State private var result = 0
+    @State private var showAlert = false
     
     var body: some View {
         VStack {
@@ -22,21 +25,44 @@ struct QuestionsView: View {
             }
             HStack {
                 ForEach(questions[currentQuestionIndex].answers, id: \.self) {answer in
-                    Button(answer, action: game)
+                    Button(answer, action: {
+                        game(chosenAnswer: answer)
+                    })
                         .padding()
-                        .bold()
                         .background(Color(red: 0.9, green: 0.8, blue: 0.8))
                         .foregroundColor(.black)
                         .cornerRadius(8)
                 }
                 .padding()
             }
-            .padding()
+            Text("The result: \(result)")
+                .bold()
+                .padding()
+        }
+        .alert("Game over", isPresented: $showAlert) {
+            Button("Retry") {
+                result = 0
+                currentQuestionIndex = 0
+                questionNr = 0
+            }
         }
     }
     
-    func game() {
-        
+    func changeQuestion() {
+        currentQuestionIndex += 1
+        questionNr += 1
+    }
+    
+    func game(chosenAnswer: String) {
+        if questionNr == 10 {
+            showAlert = true
+        } else {
+            if chosenAnswer == questions[currentQuestionIndex].answer {
+                result += 1
+            }
+            
+            changeQuestion()
+        }
     }
 }
 
