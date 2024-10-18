6a93c47990ab2492a98caf6361a9934e1e5085c8
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-18 13:27:44 +0200

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Natalia Nikiforuk on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
